45463200f2f112930a27f2be6e0e474806f998df
Christopher <christopher.bondier@etu.univ-lyon1.fr>
2023-12-27 21:48:49 +0100

    partie "a propos"

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+document.getElementById("lien_a_propos").addEventListener("click", () => {
+  document.getElementById("a_propos").scrollIntoView();
+});
+
+document.getElementById("lien_projets").addEventListener("click", () => {
+  document.getElementById("projets").scrollIntoView();
+});
+
+document.getElementById("lien_competences").addEventListener("click", () => {
+  document.getElementById("competences").scrollIntoView();
+});
+
+document.getElementById("bouton_header").addEventListener("click", () => {
+  document.getElementById("contact").scrollIntoView();
+});
